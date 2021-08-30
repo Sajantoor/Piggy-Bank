a6d306397b8f1d4e70f9422b2a3f2bde3576a327
Sajan Toor <sajantoor98@gmail.com>
2021-08-29 20:46:59 -0700

    Revert ":sparkles: Init windows"
    This reverts commit c477c74ec716601cd5e13083479b2ff5beacb099.

--- index.native.js
+++ index.native.js
@@ -2,5 +2,4 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './src/app.json';
 
-console.log('register');
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 const App = () => {
-
-  console.log('Hello World');
   return (
     <View>
       <Text>Hello, World</Text>
