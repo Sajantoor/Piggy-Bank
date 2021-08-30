c477c74ec716601cd5e13083479b2ff5beacb099
Sajan Toor <sajantoor98@gmail.com>
2021-08-29 19:33:55 -0700

    :sparkles: Init windows

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './src/app.json';
 
+console.log('register');
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 const App = () => {
+
+  console.log('Hello World');
   return (
     <View>
       <Text>Hello, World</Text>
