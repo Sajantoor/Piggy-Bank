84deb02481d1e95932d15331d006b63ff54f1ff2
Sajan Toor <sajantoor98@gmail.com>
2022-03-27 19:03:46 -0700

    Update Create React App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import name from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, { rootTag: document.getElementById('root') });
+AppRegistry.registerComponent(name, () => App);
+AppRegistry.runApplication(name, { rootTag: document.getElementById('root') });
