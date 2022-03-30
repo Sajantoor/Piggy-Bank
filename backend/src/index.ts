cdb2c1b7dfe4f47c61b49be907ead3d9bbbe613a
Sajan Toor <sajantoor98@gmail.com>
2022-03-29 22:24:00 -0700

    feat: Session based authentication
    * Use express session and redis to create and store a cookie
    that is used to authenticate the user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,67 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import express from "express";
-import { ApolloServer } from "apollo-server-express"
-import { buildSchema } from "type-graphql";
-import TransactionResolver from "./resolvers/Transaction";
-import UserResolver from "./resolvers/User";
-import connection from "./utilities/connection";
+import 'reflect-metadata';
+import 'dotenv/config';
+import { createConnection } from 'typeorm';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import TransactionResolver from './resolvers/Transaction';
+import UserResolver from './resolvers/User';
+import connection from './utilities/connection';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { createClient } from 'redis';
+import { COOKIE_NAME } from './utilities/constants';
+import { __PROD__ } from '../../frontend/src/shared/constants';
+import { Context } from './types/Context';
 
 const main = async () => {
-    // create connection to typeorm 
+    // create connection to typeorm
     await createConnection(connection);
     // create express server
     const app = express();
+
+    // create redis client
+    const RedisStore = connectRedis(session);
+    const redisClient = createClient({ legacyMode: true });
+    redisClient.connect().catch(console.error);
+
+    // Create session middleware with redis
+    app.use(
+        session({
+            name: COOKIE_NAME,
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+                httpOnly: true,
+                sameSite: 'lax', // csrf
+                secure: __PROD__, // cookie only works in https
+            },
+            saveUninitialized: false,
+            secret: process.env.SESSION_SECRET!,
+            resave: false,
+        }),
+    );
 
     // graphql schema with apollo-server
     const apolloserver = new ApolloServer({
         schema: await buildSchema({
             resolvers: [TransactionResolver, UserResolver],
-            validate: false,
         }),
+        context: ({ req, res }): Context => ({ req, res }),
     });
 
     // apply middleware and start server
     await apolloserver.start();
-    apolloserver.applyMiddleware({ app });
+    apolloserver.applyMiddleware({
+        app,
+    });
 
     app.listen(4000, () => {
         console.log('Listening on port 4000');
-    })
+    });
 };
 
 main();
--- backend/src/resolvers/User.ts
+++ backend/src/resolvers/User.ts
@@ -1,10 +1,11 @@
-import { Resolver, Arg, Mutation } from "type-graphql";
+import { Resolver, Arg, Mutation, Ctx } from "type-graphql";
 import { User } from "../entity/User";
 import argon2 from "argon2";
 import FieldError from "../types/objectTypes/FieldError";
 import userInput from "../types/inputTypes/userInput";
 import UserResponse from "../types/objectTypes/UserResponse";
 import validateInput from "../utilities/validateUsername";
+import { Context } from "../types/Context";
 
 @Resolver()
 class UserResolver {
@@ -39,7 +40,7 @@ class UserResolver {
      * @returns The user if successful, or the errors if not
      */
     @Mutation(() => UserResponse)
-    async login(@Arg("options") input: userInput): Promise<UserResponse> {
+    async login(@Arg("options") input: userInput, @Ctx() { req, res }: Context): Promise<UserResponse> {
         // usernames are case insensitive
         const username = input.username.toLowerCase();
 
@@ -60,7 +61,23 @@ class UserResolver {
             }
         }
 
+        req.session.userId = user.id;
         return { user };
+    }
+
+    @Mutation(() => UserResponse)
+    async profile(@Ctx() { req }: Context): Promise<UserResponse> {
+        if (req.session.userId == null) {
+            return {
+                errors: [new FieldError("", "You are not logged in")]
+            }
+        }
+
+        const user = await User.findOne(req.session.userId)
+        console.log(user)
+        return {
+            user: user
+        }
     }
 }
 
